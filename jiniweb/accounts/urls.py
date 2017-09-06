b2b926607c6ca16f5b6e9d552157aa1d1ad21fad
mtrl1514 <mtrl1514@gmail.com>
2017-09-06 16:47:44 +0900

    accounts app urls.py 파일 추가

---
+++ jiniweb/accounts/urls.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+import app.forms
+
+urlpatterns = [
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log In',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+]
