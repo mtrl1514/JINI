0570a5e14cc5486bae6f87b5608a5f96e2073ea0
mtrl1514 <mtrl1514@gmail.com>
2017-09-25 13:11:58 +0900

    add internationalization i18n

--- jiniweb/masterdatas/admin.py
+++ jiniweb/masterdatas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.translation import ugettext_lazy as _
 # Register your models here.
 
 # Load Customer Model
@@ -7,7 +7,19 @@ from .models import Customer
 
 # Create CustomerAdmin Class to view admin page
 class CustomerAdmin(admin.ModelAdmin):
+  exclude = (
+  'cus_cmf_1', 
+  'cus_cmf_2', 
+  'cus_cmf_3', 
+  'cus_cmf_4', 
+  'cus_cmf_5', 
+  'cus_cmf_6', 
+  'cus_cmf_7', 
+  'cus_cmf_8', 
+  'cus_cmf_9', 
+  'cus_cmf_10',)
   list_display = ('customer_id', 'customer_name', 'owner', 'create_user', 'create_time', 'update_user', 'update_time')
+  
 
 # register CustomerAdimin clasee on admin page
 admin.site.register(Customer, CustomerAdmin)
--- jiniweb/masterdatas/apps.py
+++ jiniweb/masterdatas/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
-
+# MasterData Class
 class MasterdatasConfig(AppConfig):
-    name = 'masterdatas'
+    name = 'masterdatas'    
+    verbose_name = _('Masterdata')
+    verbose_name_plural = _('Masterdatas')
+
